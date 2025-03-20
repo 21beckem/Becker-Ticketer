184b1a399b21aa505a6b53f3ec43f588d0a50673
Michael Becker <51341849+21beckem@users.noreply.github.com>
2025-03-20 14:27:45 -0600

    Add files via upload

---
+++ GenAuth.js
@@ -0,0 +1,184 @@
+class GenAuth {
+    constructor(connectingUpdatesCallback=(val) =>{console.log(val)}) {
+        this.connectingUpdatesCallback = connectingUpdatesCallback;
+        this.redirectUri = window.location.protocol + "//" + window.location.hostname + window.location.pathname;
+        console.log("***** RedirectURI *****: " + this.redirectUri);
+        
+        this.integrationQueryString = "";
+        if ( window.location.search.length !== 0 ) {
+            this.connectingUpdatesCallback("Authenticating...");
+            this.integrationQueryString = window.location.search.substring(1);
+        } else if ( window.location.hash.length !== 0 ) {
+            this.connectingUpdatesCallback("Authenticated!");
+            this.integrationQueryString = window.location.hash.substring(1);
+        }
+        this.appParams = this.parseAppParameters(this.integrationQueryString);
+        
+        // Create and initialize an instance of Client App SDK
+        this.myClientApp = new window.purecloud.apps.ClientApp({
+            gcHostOrigin: this.appParams.gcHostOrigin,
+            gcTargetEnv: this.appParams.gcTargetEnv
+        });
+        
+        // Log the Genesys Cloud environment (i.e. AWS Region) and Client Apps SDK version
+        console.log("Genesys Cloud Client App SDK Environment: " + this.myClientApp.gcEnvironment);
+        console.log("Genesys Cloud Client App SDK Version: " + this.myClientApp.version);
+        
+        // Create Genesys Cloud Platform SDK instance
+        let platformClient = require('platformClient');
+        this.client = platformClient.ApiClient.instance;
+        
+        // Point the Platform SDK to make SDK requests to the proper region for the org
+        console.log("Initializing platform client for region returned by Client App SDK: " + this.myClientApp.gcEnvironment);
+        this.client.setEnvironment(this.myClientApp.gcEnvironment);
+        
+        // Set to store OAuth access token in local storage to prevent user from being
+        // prompted for user credentials multiple times during use of the widget.
+        this.client.setPersistSettings(true, 'BeckerTicketerInteractionWidget');
+        
+        // Specific Platform SDK items utilized by this application
+        this.usersApi = new platformClient.UsersApi();
+        this.notificationsApi = new platformClient.NotificationsApi();
+        this.conversationsApi = new platformClient.ConversationsApi();
+        
+        this.userMe = null;
+        this.conversation = null;
+        
+        this.initializeApplication();
+        
+        //
+        // Lifecycle Events
+        //
+        this.myClientApp.lifecycle.addBootstrapListener(() => {
+            this.logLifecycleEvent('App Lifecycle Event: bootstrap', true);
+            this.initializeApplication();
+        });
+        this.myClientApp.lifecycle.addStopListener(() => {
+            this.logLifecycleEvent('App Lifecycle Event: stop', true);
+        
+            // Clean up other, persistent listeners
+            this.myClientApp.lifecycle.removeFocusListener(this.onAppFocus);
+            this.myClientApp.lifecycle.removeBlurListener(this.onAppBlur);
+        
+            this.myClientApp.lifecycle.stopped();
+        
+            this.myClientApp.alerting.showToastPopup(
+                'Becker Ticketer',
+                'App Stopped', {
+                    id: 'Becker-Ticketer-statusMsg',
+                    type: 'error',
+                    showCloseButton: true
+                }
+            );
+        
+            this.logLifecycleEvent('Notified Genesys Cloud of Successful App Stop', false);
+        });
+        this.myClientApp.lifecycle.addFocusListener(this.onAppFocus);
+        this.myClientApp.lifecycle.addBlurListener(this.onAppBlur);
+    }
+    logLifecycleEvent(logText, incommingEvent) { console.log(logText) };
+    showToast(message) { this.myClientApp.alerting.showToastPopup( 'Becker Ticketer', message, { id: 'Becker-Ticketer-statusMsg' } ); }
+    onAppFocus () {
+        this.logLifecycleEvent('App Lifecycle Event: focus', true);
+        this.showToast('App Focused');
+    }
+    onAppBlur () {
+        this.logLifecycleEvent('App Lifecycle Event: blur', true);
+        this.showToast('App Blurred');
+    }
+    parseAppParameters(queryString) {
+        console.log("Interaction Widget Proxy Query String: " + queryString);
+    
+        let appParams = {
+            gcHostOrigin: null,
+            gcTargetEnv: null,
+            gcLangTag: null,
+            gcConversationId: null,
+            clientId: null
+        };
+    
+        if ( queryString.length != 0 ) {
+            const pairs = queryString.split('&');
+    
+            for (var i = 0; i < pairs.length; i++)
+            {
+                var currParam = pairs[i].split('=');
+    
+                if (currParam[0] === 'gcLangTag') {
+                    appParams.gcLangTag = decodeURIComponent(currParam[1]);
+                    console.log("Query Parameter gcLangTag = " + appParams.gcLangTag);
+                } else if (currParam[0] === 'gcHostOrigin') {
+                    appParams.gcHostOrigin = decodeURIComponent(currParam[1]);
+                    console.log("Query Parameter gcHostOrigin = " + appParams.gcHostOrigin);
+                } else if (currParam[0] === 'gcTargetEnv') {
+                    appParams.gcTargetEnv = decodeURIComponent(currParam[1]);
+                    console.log("Query Parameter gcTargetEnv = " + appParams.gcTargetEnv);
+                } else if (currParam[0] === 'gcConversationId') {
+                    appParams.gcConversationId = decodeURIComponent(currParam[1]);
+                    console.log("Query Parameter gcConversationId = " + appParams.gcConversationId);
+                } else if (currParam[0] === 'ClientId') {
+                    appParams.clientId = decodeURIComponent(currParam[1]);
+                    console.log("Query Parameter ClientId = " + appParams.clientId);
+                } else if (currParam[0] === 'state') {
+                    console.log("Found 'state' query parameter from implicit grant redirect");
+                    var stateValue = currParam[1];
+                    console.log("state = " + stateValue);
+                    var stateValueDecoded = decodeURIComponent(stateValue);
+                    console.log("decoded state = " + stateValueDecoded);
+                    appParams = this.parseAppParameters(decodeURIComponent(stateValueDecoded));
+                }
+            }
+        }
+        return appParams;
+    };
+    initializeApplication() {
+        console.log("Performing application bootstrapping");
+    
+        // Perform Implicit Grant Authentication
+        //
+        // Note: Pass the query string parameters in the 'state' parameter so that they are returned
+        //       to us after the implicit grant redirect.
+        this.client.loginImplicitGrant(this.appParams.clientId, this.redirectUri, { state: this.integrationQueryString })
+            .then((data) => {
+                // User Authenticated
+                console.log("User Authenticated: " + JSON.stringify(data));
+    
+                this.connectingUpdatesCallback("Querying User...");
+    
+                // Make request to GET /api/v2/users/me?expand=presence
+                return this.usersApi.getUsersMe({ 'expand': ["presence","authorization"] });
+            })
+            .then((userMe) => {
+                // Me Response
+                this.userMe = userMe;
+    
+                this.connectingUpdatesCallback("Connected Username: " + this.userMe.username);
+    
+                this.connectingUpdatesCallback("Querying Conversation...");
+    
+                console.log("Getting initial conversation details for conversation ID: " + this.appParams.gcConversationId);
+                return this.conversationsApi.getConversation(this.appParams.gcConversationId);
+            }).then((data) => {
+                console.log("Conversation details for " + this.appParams.gcConversationId + ": " + JSON.stringify(data));
+                this.connectingUpdatesCallback( JSON.stringify(data, null, 3) );
+                this.conversation = data;
+    
+                this.myClientApp.lifecycle.bootstrapped();
+    
+                this.myClientApp.alerting.showToastPopup(
+                    'Becker Ticketer',
+                    'Bootstrap Complete', {
+                        id: 'Becker-Ticketer-statusMsg',
+                        type: 'success'
+                    }
+                );
+    
+                this.logLifecycleEvent('Notified Genesys Cloud of Successful App Bootstrap', false);
+            }).catch((err) => {
+                this.connectingUpdatesCallback("Error: See Console");
+                // Handle failure response
+                console.log(err);
+                showToast(err.message);
+            });
+    }
+}
---
+++ script.js
@@ -0,0 +1,156 @@
+HTMLCollection.prototype.forEach = Array.from(this).forEach;
+let AccordianSections = document.getElementsByClassName("accordion");
+const _ = (el) => document.getElementById(el);
+AccordianSections.forEach((el) => {
+    if (el.classList.contains("active")) {
+        el.nextElementSibling.style.height = (document.body.scrollHeight - 165) + "px";
+    }
+    el.open = (yesOrNo) => {
+        if (yesOrNo) {
+            el.classList.add("active");
+            el.nextElementSibling.style.height = (document.body.scrollHeight - 165) + "px";
+        } else {
+            el.classList.remove("active");
+            el.nextElementSibling.style.height = null;
+        }
+    };
+    el.setActive = () => {
+        // first check if they can open this section yet
+        if (canIopenThisAccordionSection(el.id)) {
+            AccordianSections.forEach((e) => e.open(false));
+            el.open(true);
+        }
+    }
+    el.addEventListener("click", el.setActive );
+});
+function canIopenThisAccordionSection(elId) {
+    if (elId == 'IdentifyAccordionBtn') return true; // always allow Identify tab to open
+    if (elId == 'AssistAccordionBtn') {
+        if (SelectedPersonId == '') return false; // can't open Assist tab if no person is selected
+        return true;
+    }
+    if (elId == 'ReviewAccordionBtn') {
+        if (SelectedPersonId == '' || !AI_TicketHasBeenGenerated) return false; // can't open Review tab if no person is selected and if no KB article is selected
+        return true;
+    }
+    return false;
+}
+// set transition AFTER opening the first section
+document.documentElement.style.setProperty('--accordion-transition', 'height 0.5s ease');
+// on doument resize, re-set height property
+const resizeToWindowHeight = () => {
+    document.querySelector('.accordion.active + .accordion-panel').style.height = (document.body.scrollHeight - 170) + "px";
+}
+document.addEventListener("click", (e) => resizeToWindowHeight);
+window.addEventListener('resize', resizeToWindowHeight);
+// register input onenter
+Array.from(document.querySelectorAll('*[onenter]')).forEach((el) => {
+    el.addEventListener("keyup", (e) => {
+        // console.log(e.keyCode);
+        if (e.keyCode === 13) {
+            _(el.getAttribute('onenter')).click();
+        }
+    });
+});
+
+function detectSearchQueryType(inVal, outputSpan) {
+    inVal = inVal.trim().toLowerCase();
+    if (inVal == '') {
+        outputSpan.innerText = '...';
+        _('IdenifySearchButton').disabled = true;
+        _('IdenifySearchQuery').setAttribute('Q-type', 'X');
+    }
+    // if inVal is an I-number
+    else if (inVal.length == 9 && !isNaN(inVal)) {
+        outputSpan.innerText = 'I-Number';
+        _('IdenifySearchButton').disabled = false;
+        _('IdenifySearchQuery').setAttribute('Q-type', 'I#');
+    }
+    // if inVal starts with + and has only numbers, parenthesis, hyphens, and spaces
+    else if (inVal.startsWith('+') && inVal.match(/^[+0-9()-\s]*$/)) {
+        outputSpan.innerText = 'Phone Number';
+        _('IdenifySearchButton').disabled = false;
+        _('IdenifySearchQuery').setAttribute('Q-type', 'PHONE');
+    }
+    // if inval is a name
+    else {
+        outputSpan.innerText = 'Name';
+        _('IdenifySearchButton').disabled = false;
+        _('IdenifySearchQuery').setAttribute('Q-type', 'NAME');
+    }
+}
+function showLoader(yesOrNo) {
+    _('loadingOverlay').style.display = yesOrNo ? null : 'none';
+}
+
+let PersonSearchResults = [];
+let SelectedPersonId = '';
+let AI_TicketHasBeenGenerated = false;
+async function conductIdenitySearch(el) {
+    let q = el.value;
+    let t = el.getAttribute('Q-type');
+    if (q.trim() == '' || t == 'X') return;
+
+    // reset the selection process
+    SelectedPersonId = '';
+    PersonSearchResults = [];
+    AI_TicketHasBeenGenerated = false;
+
+    showLoader(true);
+
+    await new Promise(r => setTimeout(r, (Math.random() * 1000) + 1000));
+    
+    showLoader(false);
+
+    PersonSearchResults = [
+        ['020bfd25-06c8-ee11-9f01-c89665346b33', 'Michael Becker', 'bec24007@byui.edu', 'BYUI', '', 'OnCampusStudent', 'Yes', 'User', '', 'Sat 2/10/24 4:18 AM'],
+        ['bae72298-d6bb-ec11-997e-c89665346b33', 'Aleah Clyde', 'cly22001@byui.edu', 'BYUI', '', 'OnCampusStudent', 'Yes', 'User', '', 'Sat 2/10/24 4:18 AM'],
+        ['f65f2ca2-efe9-ea11-9112-005056ac5ec6', 'Daniel Mann', 'man21018@byui.edu', 'BYUI', '', 'OnCampusStudent', 'Yes', 'User', '', 'Sat 8/29/20 4:03 AM']
+    ];
+
+    // display Results
+    const peopleReultsList = _('peopleReultsList');
+    peopleReultsList.innerHTML = '';
+    PersonSearchResults.forEach((result) => {
+        let personCard = document.createElement('div');
+        personCard.className = 'personCard';
+        personCard.setAttribute('id', result[0]);
+        personCard.innerHTML = `
+            <h3 onclick="openPersonDetails('`+result[0]+`')">`+result[1]+`</h3>
+            <p>`+result[2]+`</p>
+            <p>`+result[3]+`</p>
+            <button onclick="selectPersonAndStartTicket('`+result[0]+`')"><div style="width: 30px; height: 100%; display: flex; align-items: center;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="var(--gen-orange)"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M64 64C28.7 64 0 92.7 0 128l0 64c0 8.8 7.4 15.7 15.7 18.6C34.5 217.1 48 235 48 256s-13.5 38.9-32.3 45.4C7.4 304.3 0 311.2 0 320l0 64c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-64c0-8.8-7.4-15.7-15.7-18.6C541.5 294.9 528 277 528 256s13.5-38.9 32.3-45.4c8.3-2.9 15.7-9.8 15.7-18.6l0-64c0-35.3-28.7-64-64-64L64 64zm64 112l0 160c0 8.8 7.2 16 16 16l288 0c8.8 0 16-7.2 16-16l0-160c0-8.8-7.2-16-16-16l-288 0c-8.8 0-16 7.2-16 16zM96 160c0-17.7 14.3-32 32-32l320 0c17.7 0 32 14.3 32 32l0 192c0 17.7-14.3 32-32 32l-320 0c-17.7 0-32-14.3-32-32l0-192z"/></svg></div></button>
+        `;
+        peopleReultsList.appendChild(personCard);
+    });
+}
+let currentPopupWindow = null;
+async function openPersonDetails(U_identifier) {
+    const popupWidth = 800;
+    const popupHeight = 760;
+    const left = (screen.width - popupWidth) / 2;
+    const top = (screen.height - popupHeight) / 2;
+    currentPopupWindow = window.open('https://td.byui.edu/TDNext/Apps/People/PersonDet.aspx?U=' + U_identifier, 'PersonDetails', 'width='+popupWidth+',height='+popupHeight+',top='+top+',left='+left);
+}
+
+function selectPersonAndStartTicket(U_identifier) {
+    // highlight that person's card
+    SelectedPersonId = U_identifier;
+    _('peopleReultsList').children.forEach(personCard => personCard.classList.remove('selected'));
+    document.querySelector('#peopleReultsList div.personCard[id="'+SelectedPersonId+'"]').classList.add('selected');
+    
+    let personArray = PersonSearchResults.filter((per) => per[0] === SelectedPersonId)[0];
+    _('RequesterName_toSubmit').value = personArray[1];
+    document.querySelector('#IdentifyAccordionBtn span').innerHTML = ': ' + personArray[1];
+    _('AssistAccordionBtn').setActive();
+}
+
+
+async function generateTicketWithAI() {
+    showLoader(true);
+    await new Promise(r => setTimeout(r, (Math.random() * 1000) + 1000));
+    AI_TicketHasBeenGenerated = true;
+    showLoader(false);
+
+    _('ReviewAccordionBtn').setActive();
+}
